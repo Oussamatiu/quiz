ef6608ab580253a46f164e62c21203c1300192f0
oussama <oussamachentouf864@gmail.com>
2025-11-02 18:34:26 +0100

    add some style and more logic about buttons next and back

--- indax.js
+++ indax.js
@@ -1,71 +1,97 @@
 const questionX = document.getElementById("question");
 const firstQuestion = document.createElement("h3");
+const startButton = document.createElement('button');
+startButton.setAttribute('class' , 'btn-start');
+startButton.textContent = "start quiz";
+startButton.addEventListener('click',printQuestion);
+questionX.appendChild(startButton);
+const btnControle = document.createElement('div');
+const groupOption = document.createElement('div');
+groupOption.setAttribute('class', 'options-group');
 let count = 0;
 let score = 0;
 let valide = 0;
-function printQuestion(){
-      restart();
-      firstQuestion.textContent = questions[count].question;
-      questionX.append(firstQuestion);
-      questions[count].options.forEach(option => {
+
+
+function printQuestion() {
+ restart();
+  if (count >= questions.length) {
+    const resulta = document.createElement('h3');
+    resulta.textContent = `your score is ${score}/${questions.length}`;
+    questionX.appendChild(resulta);
+
+  }
+  else {
+    firstQuestion.textContent = questions[count].question;
+    questionX.append(firstQuestion);
+    questions[count].options.forEach(option => {
       const button = document.createElement('button');
       button.textContent = option;
-      button.setAttribute("class" , 'btn');
-      button.addEventListener('click', function check(e){
-        document.querySelectorAll('.btn').forEach(item =>{
-            item.classList.remove('active');
+      button.setAttribute("class", 'btn');
+      if (questions[count].reponse === option) {
+        button.classList.add('active');
+      }
+
+        button.addEventListener('click', function check(e) {
+        nextButton.style.visibility = 'visible'
+        document.querySelectorAll('.btn').forEach(item => {
+          item.classList.remove('active');
         });
         e.target.classList.add('active');
         questions[count].reponse = button.textContent;
-        let x = e.target.value;
         console.log(questions[count].reponse);
-        if(option === questions[count].correct){
-            valide = 1;
-        }else{
-            valide = 0;
+        if (questions[count].reponse === questions[count].correct) {
+          valide = 1;
+        } else {
+          valide = 0;
         }
+
       })
-      questionX.appendChild(button);
+      groupOption.appendChild(button);
+      questionX.appendChild(groupOption);
+
     })
-    if(questions[count].reponse != ""){
-      questions[count].options.forEach(option => {
-        if(option === questions[count].reponse){
-         
-          const x = document.querySelector('.btn') =;
-          console.log(x);
-          x.classList.add('active');
-          
-          
-        }
-      })
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'next';
+    nextButton.style.display = 'block';
+    const checkActive = document.querySelector('.active');
+    if(!checkActive){
+      nextButton.style.visibility = 'hidden'
     }
-     const nextButton = document.createElement('button');
-      nextButton.textContent = 'next';
-      nextButton.style.display = 'block';
-      questionX.appendChild(nextButton);
-      const preversButton = document.createElement('button');
-      preversButton.textContent = 'prevers';
-      questionX.appendChild(preversButton);
-      preversButton.addEventListener('click', ()=> {
-        --count;
-        printQuestion();
-      })
-      nextButton.addEventListener('click', () => {
-        if(valide){
-            score+=1;
-            console.log(score);
-        }
-        ++count;
-        printQuestion();
-      })
     
+
+  
+    btnControle.setAttribute('class','btn-controle');
+    const preversButton = document.createElement('button');
+    preversButton.textContent = 'prevers';
+    btnControle.appendChild(preversButton);
+    if( count == 0){
+      preversButton.style.visibility = 'hidden';
+    }else {
+      preversButton.style.visibility = 'visible';
+    }
+    btnControle.appendChild(nextButton);
+    questionX.appendChild(btnControle);
+    preversButton.addEventListener('click', () => {
+      --count;
+      printQuestion();
+    })
+    nextButton.addEventListener('click', () => {
+      if (valide) {
+        score += 1;
+        console.log(score);
+      }
+      ++count;
+      printQuestion();
+    })
+  }
 }
 
-function restart(){
-    const elements = questionX.querySelectorAll("button");
-    elements.forEach(item => {
-        item.remove();
-    })
+function restart() {
+  const elements = questionX.querySelectorAll("button , h3");
+  elements.forEach(item => {
+    item.remove();
+  })
 }
-printQuestion();
-printQuestion();
+
+
